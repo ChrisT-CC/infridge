484d70f11a725b4073a7bd1a6a81cc597a4e63fa
ChrisT-CC <cristaecc@gmail.com>
2022-03-06 19:45:50 +0000

    Create a function to check if the ingredients are available

--- run.py
+++ run.py
@@ -40,9 +40,6 @@ ingredients_worksheet = SHEET.worksheet("ingredients")  # Ingredients worksheet
 ingredients_list = ingredients_worksheet.col_values(1)  # List of ingredients
 ingredients_quantity = ingredients_worksheet.col_values(2)
 
-# print(data)
-# print(ingredients_list)
-
 
 def get_basic_ingredient():
     '''
@@ -55,16 +52,14 @@ def get_basic_ingredient():
 
     while True:
         basic_ingredient = input("Please choose a basic ingredient: ")
-        # print(f"The basic ingredient you chose is: {basic_ingredient}")
         if basic_ingredient.isdigit():
             print("The basic ingredient can't be a number. Try again\n")
         elif basic_ingredient == "":
             print("The basic ingredient can't empty. Try again\n")
         else:
-            # print(f"{basic_ingredient} is a string"+"\n")
             if validate_basic_ingredient(basic_ingredient):
-                # print(f"'{basic_ingredient}' is valid")
                 break
+
     return basic_ingredient
 
 
@@ -77,6 +72,7 @@ def validate_basic_ingredient(value):
     else:
         print(f"'{value}' is not in the list of ingredients"+"\n")
         return False
+
     return True
 
 
@@ -92,14 +88,12 @@ def generate_available_recipes_list(value):
     '''Generate a list of recipes that contains the chosen basic ingredient'''
     print(f"Generating the list of recipes based on {value} ..."+"\n")
     all_recipes = recipes.col_values(1)
-    # print(all_recipes)
     print(f"Your recipes with {value} are:")
     ingredient_recipes = [
         recipes.cell(num, 1).value
         for num in range(1, len(all_recipes)+1)
         if value in recipes.row_values(num)
         ]
-    # print(ingredient_recipes)
 
     return ingredient_recipes
 
@@ -111,8 +105,7 @@ def print_available_recipes(value):
     for ind in range(num):
         print(f"{ind+1} {value[ind]}")
         recipes_dict.update({ind+1: value[ind]})
-        # print(recipes_dict)
-    # print(recipes_dict)
+
     return recipes_dict
 
 
@@ -137,26 +130,48 @@ def validate_choice(choice_num, max_num):
         int(choice_num)
         if int(choice_num) > max_num:
             raise ValueError(f"Choose a number lower then {max_num+1}")
-            # print("validate_choice")
     except ValueError as error:
         print(f"Invalid data: {error}, please try again.\n")
         return False
+
     return True
 
 
 def get_ingredients(rec_r):
     """Get ingredients for chosen recipe"""
     ing_list = rec_r[1:-1]
-    print(ing_list)
+
     return ing_list
+
+
+def ingredients_infridge(ing_list):
+    """Check ingredients for chosen recipe exist"""
+    print("Checking ingredients...\n")
+    missing_ingredients = []
+    for ing in ing_list:
+        ing_cell_num = ingredients_worksheet.find(ing).row
+        ing_quant = ingredients_quantity[ing_cell_num-1]
+        if ing_quant == "0":
+            missing_ingredients.append(ing)
+        else:
+            # print(f"{ing} = {ing_quant}")
+            continue
+    if missing_ingredients == []:
+        print("All ingredients available")
+        print("Printing recipe...\n")
+    else:
+        print("Recipe not available")
+        print("options")
+        print(missing_ingredients)
+    # print(missing_ingredients)
+    # return missing_ingredients
 
 
 def get_recipe_row(rec_choice):
     """Find the recipe row in recipes worksheet"""
     recipe_cell_num = recipes.find(rec_choice).row
-    # print(recipe_cell_num)
     rec_row = recipes.row_values(recipe_cell_num)
-    # print(recipe_row)
+
     return rec_row
 
 
@@ -169,4 +184,5 @@ available_recipes = generate_available_recipes_list(basic_ing)
 dict_available_recipes = print_available_recipes(available_recipes)
 recipe_choice = choose_recipe(dict_available_recipes)
 recipe_row = get_recipe_row(recipe_choice)
-get_ingredients(recipe_row)
+ingredients_list = get_ingredients(recipe_row)
+ingredients_infridge(ingredients_list)
